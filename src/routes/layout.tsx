82fde084c660cbb41f2e63f96c6ece695eebc30c
Artur Tumasyan <tumasyan1artur@gmail.com>
2023-03-28 17:20:51 +0300

    Test commit to prevent vercel issue on deployment

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,31 +1,31 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { CustomFooter } from '../integrations/react/mui';
-import Header from '../components/Header/header';
+// import { CustomFooter } from '../integrations/react/mui';
+// import Header from '../components/Header/header';
 // There are some building issues with Vercel
 // Please use these code instead of imported Header whenever you need to deploy on Vercel
-// import { Link } from '@builder.io/qwik-city'
+import { Link } from '@builder.io/qwik-city'
 
-// export const Header = component$(() => {
-//   const pages = [{ title: 'Sales', url: '/category/sales' } , { title: 'About us', url: '/about-us' }];
+export const Header = component$(() => {
+  const pages = [{ title: 'Sales', url: '/category/sales' } , { title: 'About us', url: '/about-us' }];
 
-//   return (
-//     <header class="header">
-//       <h1 class="logo"><Link href='/'>LOGO</Link></h1>
-//       <div class="menu-items">
-//         {pages?.map((item) => (
-//           <div>
-//             <Link
-//               href={item.url}
-//             >
-//               {item.title}
-//             </Link>
-//           </div>
-//         ))
-//        }
-//       </div>
-//     </header>
-//   );
-// });
+  return (
+    <header class="header">
+      <h1 class="logo"><Link href='/'>LOGO</Link></h1>
+      <div class="menu-items">
+        {pages?.map((item) => (
+          <div>
+            <Link
+              href={item.url}
+            >
+              {item.title}
+            </Link>
+          </div>
+        ))
+       }
+      </div>
+    </header>
+  );
+});
 
 export default component$(() => {
   return (
@@ -35,7 +35,7 @@ export default component$(() => {
         <section>
           <Slot />
         </section>
-        <CustomFooter />
+        {/* <CustomFooter /> */}
       </main>
     </>
   );
